e3f533ea4018c36341e90740346a99059600be0c
GRREEN <Rome.jeffery-powell@outlook.com>
2026-02-27 12:19:44 +0000

    Update GRREENCraft.js

--- GRREENCraft.js
+++ GRREENCraft.js
@@ -41,27 +41,4 @@ class GRRenderer {
 
   draw() {
     const { ctx, blockSize, world, textures } = this;
-    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    for (let x = 0; x < world.width; x++) {
-      for (let y = 0; y < world.height; y++) {
-        const block = world.blocks[x][y];
-        const tex = textures[block];
-
-        if (tex && tex.complete) {
-          ctx.drawImage(tex, x * blockSize, y * blockSize, blockSize, blockSize);
-        } else {
-          ctx.fillStyle = block === "grass" ? "#3cb043" : "#87ceeb";
-          ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
-        }
-      }
-    }
-  }
-}
-
-window.addEventListener("load", () => {
-  const canvas = document.getElementById("game");
-  const world = new GRWorld(40, 30);
-  const renderer = new GRRenderer(canvas, world);
-  renderer.draw();
-});
+    ctx.clearRect(0, 0, this.canvas
